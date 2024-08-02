d7fd40d848c0885e57ed1e9054b38d7f7918bf3d
aceransh <anshdesai@me.com>
2024-08-02 11:56:11 -0400

    still trying smh

--- copy/DB.py
+++ copy/DB.py
@@ -10,6 +10,7 @@ def create_connection():
         print(f"The error '{e}' occurred")
         return None
 
+
 # Create a table to store product information and price history
 def create_table(conn):
     try:
@@ -27,22 +28,26 @@ def create_table(conn):
     except sqlite3.Error as e:
         print(f"The error '{e}' occurred during table creation")
 
+
 # insert product data
 def insert_product_data(conn, product_data):
     with conn:
         cursor = conn.cursor()
         cursor.execute("""
-            INSERT INTO product_database (name, url, price, timestamp) 
-            VALUES(:name, :url, :price, :timestamp)""",
+            INSERT INTO product_database (name, url, price, shipped_by, sold_by, rating, timestamp) 
+            VALUES(:name, :url, :price, :shipped_by, :sold_by, :rating, :timestamp)""",
             {
                 'name': product_data.name,
                 'url': product_data.url,
                 'price': product_data.price,
+                'shipped_by': product_data.shipped_by,
+                'sold_by': product_data.sold_by,
+                'rating': product_data.rating,
                 'timestamp': product_data.timestamp
             }
         )
         return cursor.lastrowid
-
+        
 # Delete product data from the table based on the product id.
 def delete_product_data(conn, product_id):
     with conn:
@@ -63,12 +68,12 @@ def insert_test_data():
     
     # Insert test data
     cursor.execute("""
-    INSERT INTO product_database (name, url, price, timestamp) 
-    VALUES (?, ?, ?, ?)""", 
+    INSERT INTO product_database (name, url, price, shipped_by, sold_by, rating, timestamp) 
+    VALUES (?, ?, ?, ?, ?, ?, ?)""", 
     ('ASUS TUF Gaming 23.8" 1080P Monitor (VG249Q1R) - Full HD, IPS, 165Hz (Supports 144Hz), 1ms, Extreme Low Motion Blur, Speaker, FreeSync Premium, Shadow Boost, VESA Mountable, DisplayPort, HDMI', 
      'https://www.newegg.com/black-asus-tuf-gaming-vg249q1r-24/p/N82E16824281256?Item=N82E16824281256', 
      150.00, 
-     datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+    'Shipped By Test', 'Sold By Test', '4.5', datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
     
     conn.commit()
     conn.close()
